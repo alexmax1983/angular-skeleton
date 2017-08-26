8b05c4384c77d2ed17c417f3a841fbdef58242f4
Alexandr Sazanovich <alexander@denovo-digital.com>
2017-08-26 12:17:18 +0300

    init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name productListApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the productListApp
+ */
+angular.module('productListApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
