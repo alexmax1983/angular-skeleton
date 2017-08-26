cd9347e3c83855edb9315d78723f783096a86356
Alexandr Sazanovich <sazanovich.developer@gmail.com>
2017-08-26 23:30:12 +0300

    Генерация сервиса

---
+++ app/scripts/services/myservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name productListApp.myService
+ * @description
+ * # myService
+ * Service in the productListApp.
+ */
+angular.module('productListApp')
+  .service('myService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
